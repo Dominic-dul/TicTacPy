66e20cbe66abc0a793b68858de9f8e285bfaec46
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2022-08-29 20:17:28 +0300

    display board function implementation

---
+++ tictac.py
@@ -0,0 +1,12 @@
+﻿from dis import dis
+
+
+print("Hello")
+
+def display_board(board):
+    for tiles in range(0, len(board)-1, 3):
+        print(board[tiles+1:tiles+4])
+
+test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+
+display_board(test_board)
