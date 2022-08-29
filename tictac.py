9dae03e0c8888596dbb3ec3c013e66d07f6b524b
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2022-08-29 20:20:35 +0300

    display board function implementation

--- tictac.py
+++ tictac.py
@@ -8,5 +8,18 @@ def display_board(board):
         print(board[tiles+1:tiles+4])
 
 test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
-
 display_board(test_board)
+
+def player_input():
+    player1 = '#'
+    player2 = None
+
+    while player1 not in ['O', 'X']:
+        player1 = input("Please select which one are you, 'X' or 'O':")
+
+    if player1 == 'O':
+        player2 = 'X'
+    else:
+        player2 = 'X'
+
+player_input()
