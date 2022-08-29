d8195b3a4a540bd9f7e392b6411a0ae448a44ae4
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2022-08-29 21:58:11 +0300

    implement game logic

--- tictac.py
+++ tictac.py
@@ -1,15 +1,13 @@
-﻿import random
+﻿import random as ran
 
 def display_board(board):
     for tiles in range(0, len(board)-1, 3):
         print(board[tiles+1:tiles+4])
 
-test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
-#display_board(test_board)
-
 def player_input():
-    player1 = '#'
-    player2 = None
+
+    global player1
+    global player2
 
     while player1 not in ['O', 'X']:
         player1 = input("Player1, please select which one are you, 'X' or 'O':")
@@ -27,7 +25,7 @@ def player_input():
 #player_input()
 
 def place_marker(board, marker, postion):
-    board[-postion] = marker
+    board[postion] = marker
 
 #place_marker(test_board,'$',8)
 #display_board(test_board)
@@ -56,25 +54,30 @@ def win_check(board, mark):
 #print(win_check(test_board, 'O'))
 
 def choose_first():
-    choice = random.randInt(1, 2)
+    choice = ran.randint(1,2)
     if choice == 1:
         return "Player1"
     else:
         return "Player2"
 
 def space_check(board, position):
-    return board[-position] == ' ' or position in range(0,9)
+    if position in range(0,10) and board[position] == ' ':
+        return True
+    else:
+        return False
 
-def player_choicer(board):
+def player_choice(board):
     choice = 10
 
-    while choice not in range(0,9) and not space_check(board, choice):
-        choice = input("Enter the position 1-9 you want to place your marker in:")
+    while choice not in range(0,10) or not space_check(board, choice):
+        choice = int(input("Enter the position 1-9 you want to place your marker in:"))
 
-        if choice not in range(0,9):
+        if choice not in range(0,10):
             print("Wrong input! Try again")
         if not space_check(board, choice):
             print("This place is already taken. Try again")
+
+    return choice
 
 def full_board_check(board):
     for position in board:
@@ -91,3 +94,66 @@ def replay():
 
         if choice not in ['Y', 'N']:
             print("Wrong input! Try again")
+
+    return choice == 'Y'
+
+player1 = '#'
+player2 = '#'
+
+print('Welcome to Tic Tac Toe!')
+
+while True:
+    game_board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    game_board_postions = ['$', 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    marker1 = '#'
+    marker2 = '#'
+    choice = None
+
+    print("This is the positions to place in the board:")
+    display_board(game_board_postions)
+
+    player_input()
+
+    starting_player = choose_first()
+    second_player = None
+
+    if starting_player == 'Player1':
+        second_player = "Player2"
+        marker1 = player1
+        marker2 = player2
+    else:
+        second_player = "Player1"
+        marker1 = player2
+        marker2 = player1
+
+    while not full_board_check(game_board):
+        print(100*'\n')
+        print(f"{starting_player} turn:")
+        display_board(game_board)
+
+        choice = player_choice(game_board)
+        place_marker(game_board, marker1, choice)
+
+        print(100*'\n')
+        display_board(game_board)
+
+        if win_check(game_board, marker1):
+            print(f"{starting_player} has won the game!")
+            break
+
+        print(100*'\n')
+        print(f"{second_player} turn:")
+        display_board(game_board)
+
+        choice = player_choice(game_board)
+        place_marker(game_board, marker2, choice)
+
+        print(100*'\n')
+        display_board(game_board)
+
+        if win_check(game_board, marker2):
+            print(f"{second_player} has won the game!")
+            break
+
+    if not replay():
+        break
