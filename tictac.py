b5294e20a1c879c9e55bd205dd003a022336c205
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2022-08-29 20:23:16 +0300

    define player picking x or o func

--- tictac.py
+++ tictac.py
@@ -8,18 +8,23 @@ def display_board(board):
         print(board[tiles+1:tiles+4])
 
 test_board = ['#', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
-display_board(test_board)
+#display_board(test_board)
 
 def player_input():
     player1 = '#'
     player2 = None
 
     while player1 not in ['O', 'X']:
-        player1 = input("Please select which one are you, 'X' or 'O':")
+        player1 = input("Player1, please select which one are you, 'X' or 'O':")
+
+        if player1 not in ['O', 'X']:
+            print("Incorrect input! Please try again")
 
     if player1 == 'O':
         player2 = 'X'
     else:
-        player2 = 'X'
+        player2 = 'O'
+
+    print(f"Player1 is {player1}, Player2 ir {player2}")
 
 player_input()
